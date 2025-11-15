c56883d4cdc9e4c3d9b132ea1689a9372c3db8ca
Kevin Luna <L21020397@veracruz.tecnm.mx>
2025-11-14 21:20:26 -0600

    Se removieron palabras reservadas de los nombres de las columnas en los esquemas de base de datos

--- src/mysql_schema.sql
+++ src/mysql_schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE cursito;
 USE cursito;
 
 -- Tabla para almacenar los departamentos
-CREATE TABLE department (
+CREATE TABLE departments (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     name VARCHAR(100) NOT NULL
 );
 
 -- Tabla para almacenar los periodos de los cursos
-CREATE TABLE period (
+CREATE TABLE periods (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     name VARCHAR(100) NOT NULL,
     start_date DATE NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE surveys (
 );
 
 -- Tabla para almacenar la información de los trabajadores (docentes/personal)
-CREATE TABLE worker (
+CREATE TABLE workers (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     department_id CHAR(36) NOT NULL,
     rfc VARCHAR(13) NOT NULL UNIQUE,
@@ -36,14 +36,14 @@ CREATE TABLE worker (
     name VARCHAR(45) NOT NULL,
     fathers_surname VARCHAR(40) NOT NULL,
     mother_surname VARCHAR(40),
-    `role` TINYINT NOT NULL, -- 0 = docente, 1 = jefe de departamento
+    position TINYINT NOT NULL, -- 0 = docente, 1 = jefe de departamento
     CONSTRAINT fk_department
         FOREIGN KEY(department_id) 
-        REFERENCES department(id)
+        REFERENCES departments(id)
 );
 
 -- Tabla para almacenar la información de los cursos
-CREATE TABLE course (
+CREATE TABLE courses (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     period_id CHAR(36) NOT NULL,
     target VARCHAR(255) NOT NULL,
@@ -52,14 +52,14 @@ CREATE TABLE course (
     end_date DATE NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
-    `type` TINYINT NOT NULL, -- 0 = diplomado, 1 = taller
-    `mode` TINYINT NOT NULL, -- 0 = virtual, 1 = presencial
-    `profile` TINYINT NOT NULL, -- 0 = formacion, 1 = actualización docente
+    course_type TINYINT NOT NULL, -- 0 = diplomado, 1 = taller
+    modality TINYINT NOT NULL, -- 0 = virtual, 1 = presencial
+    course_profile TINYINT NOT NULL, -- 0 = formacion, 1 = actualización docente
     goal TEXT NOT NULL,
     details TEXT,
     CONSTRAINT fk_period
         FOREIGN KEY(period_id) 
-        REFERENCES period(id)
+        REFERENCES periods(id)
 );
 
 -- Tabla para almacenar las preguntas de las encuestas
@@ -67,7 +67,7 @@ CREATE TABLE questions (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     survey_id CHAR(36) NOT NULL,
     question VARCHAR(100) NOT NULL,
-    `position` SMALLINT NOT NULL,
+    question_order SMALLINT NOT NULL,
     CONSTRAINT fk_survey
         FOREIGN KEY(survey_id) 
         REFERENCES surveys(id)
@@ -80,10 +80,10 @@ CREATE TABLE instructors (
     course_id CHAR(36) NOT NULL,
     CONSTRAINT fk_instructor_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_instructor_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     UNIQUE (worker_id, course_id) -- Un instructor solo puede estar una vez en un curso
 );
 
@@ -95,10 +95,10 @@ CREATE TABLE enrollings (
     final_grade DECIMAL(5, 2),
     CONSTRAINT fk_enrolling_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_enrolling_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     UNIQUE (worker_id, course_id) -- Un trabajador solo puede inscribirse una vez al mismo curso
 );
 
@@ -107,14 +107,14 @@ CREATE TABLE attendances (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     worker_id CHAR(36) NOT NULL,
     course_id CHAR(36) NOT NULL,
-    `date` DATE NOT NULL,
+    attendance_date DATE NOT NULL,
     CONSTRAINT fk_attendance_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_attendance_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
-    UNIQUE(worker_id, course_id, `date`) -- Solo un registro de asistencia por día por trabajador por curso
+        REFERENCES courses(id),
+    UNIQUE(worker_id, course_id, attendance_date) -- Solo un registro de asistencia por día por trabajador por curso
 );
 
 -- Tabla para almacenar las respuestas a las encuestas
@@ -123,13 +123,13 @@ CREATE TABLE answers (
     worker_id CHAR(36) NOT NULL,
     course_id CHAR(36) NOT NULL,
     question_id CHAR(36) NOT NULL,
-    value TEXT NOT NULL,
+    answer TEXT NOT NULL,
     CONSTRAINT fk_answer_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_answer_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     CONSTRAINT fk_answer_question
         FOREIGN KEY(question_id) 
         REFERENCES questions(id),
--- src/pg_schema.sql
+++ src/pg_schema.sql
@@ -1,11 +1,11 @@
 -- Tabla para almacenar los departamentos
-CREATE TABLE department (
+CREATE TABLE departments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL
 );
 
 -- Tabla para almacenar los periodos de los cursos
-CREATE TABLE period (
+CREATE TABLE periods (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     start_date DATE NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE surveys (
 );
 
 -- Tabla para almacenar la información de los trabajadores (docentes/personal)
-CREATE TABLE worker (
+CREATE TABLE workers (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     department_id UUID NOT NULL,
     rfc VARCHAR(13) NOT NULL UNIQUE,
@@ -32,14 +32,14 @@ CREATE TABLE worker (
     name VARCHAR(45) NOT NULL,
     fathers_surname VARCHAR(40) NOT NULL,
     mother_surname VARCHAR(40),
-    "role" SMALLINT NOT NULL, -- 0 = docente, 1 = jefe de departamento
+    position SMALLINT NOT NULL, -- 0 = docente, 1 = jefe de departamento
     CONSTRAINT fk_department
         FOREIGN KEY(department_id) 
-        REFERENCES department(id)
+        REFERENCES departments(id)
 );
 
 -- Tabla para almacenar la información de los cursos
-CREATE TABLE course (
+CREATE TABLE courses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     period_id UUID NOT NULL,
     target VARCHAR(255) NOT NULL,
@@ -48,14 +48,14 @@ CREATE TABLE course (
     end_date DATE NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
-    "type" SMALLINT NOT NULL, -- 0 = diplomado, 1 = taller
-    "mode" SMALLINT NOT NULL, -- 0 = virtual, 1 = presencial
-    "profile" SMALLINT NOT NULL, -- 0 = formacion, 1 = actualización docente
+    course_type SMALLINT NOT NULL, -- 0 = diplomado, 1 = taller
+    modality SMALLINT NOT NULL, -- 0 = virtual, 1 = presencial
+    course_profile SMALLINT NOT NULL, -- 0 = formacion, 1 = actualización docente
     goal TEXT NOT NULL,
     details TEXT,
     CONSTRAINT fk_period
         FOREIGN KEY(period_id) 
-        REFERENCES period(id)
+        REFERENCES periods(id)
 );
 
 -- Tabla para almacenar las preguntas de las encuestas
@@ -63,7 +63,7 @@ CREATE TABLE questions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     survey_id UUID NOT NULL,
     question VARCHAR(100) NOT NULL,
-    "position" SMALLINT NOT NULL,
+    question_order SMALLINT NOT NULL,
     CONSTRAINT fk_survey
         FOREIGN KEY(survey_id) 
         REFERENCES surveys(id)
@@ -76,10 +76,10 @@ CREATE TABLE instructors (
     course_id UUID NOT NULL,
     CONSTRAINT fk_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     UNIQUE (worker_id, course_id) -- Un instructor solo puede estar una vez en un curso
 );
 
@@ -91,10 +91,10 @@ CREATE TABLE enrollings (
     final_grade DECIMAL(5, 2),
     CONSTRAINT fk_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     UNIQUE (worker_id, course_id) -- Un trabajador solo puede inscribirse una vez al mismo curso
 );
 
@@ -103,14 +103,14 @@ CREATE TABLE attendances (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     worker_id UUID NOT NULL,
     course_id UUID NOT NULL,
-    "date" DATE NOT NULL,
+    attendance_date DATE NOT NULL,
     CONSTRAINT fk_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
-    UNIQUE(worker_id, course_id, "date") -- Solo un registro de asistencia por día por trabajador por curso
+        REFERENCES courses(id),
+    UNIQUE(worker_id, course_id, attendance_date) -- Solo un registro de asistencia por día por trabajador por curso
 );
 
 -- Tabla para almacenar las respuestas a las encuestas
@@ -122,10 +122,10 @@ CREATE TABLE answers (
     value TEXT NOT NULL,
     CONSTRAINT fk_worker
         FOREIGN KEY(worker_id) 
-        REFERENCES worker(id),
+        REFERENCES workers(id),
     CONSTRAINT fk_course
         FOREIGN KEY(course_id) 
-        REFERENCES course(id),
+        REFERENCES courses(id),
     CONSTRAINT fk_question
         FOREIGN KEY(question_id) 
         REFERENCES questions(id),
